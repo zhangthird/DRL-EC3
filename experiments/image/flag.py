18c1848965849b54bf653b7d294f83e3a0c8bff2
Carl <95128620+zhangthird@users.noreply.github.com>
2025-04-14 14:20:57 +0800

    flag更新

--- experiments/image/flag.py
+++ experiments/image/flag.py
@@ -1,6 +1,6 @@
 # 替换 tf.app.flags 的导入
 from absl import flags as tf_flags
-import tensorflow as tf
+# import tensorflow as tf
 
 # 使用 absl.flags 替代 tf.app.flags
 flags = tf_flags
@@ -15,3 +15,7 @@ flags.DEFINE_integer('fill_value', -1, 'the value of FillStation')
 
 flags.DEFINE_integer('map_x', 16, 'the length of x-axis')
 flags.DEFINE_integer('map_y', 16, 'the length of y-axis')
+
+# 添加以下代码来确保在导入时标志被解析
+# 这样当其他模块导入此模块时，标志已经被解析
+FLAGS.mark_as_parsed()
